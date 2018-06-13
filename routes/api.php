38a3d599725b25c72cbc77f9c02ee18f48907de4
CHIK SUM <chiksum.wong@gmail.com>
2018-06-13 22:18:36 +0800

    id version

--- routes/api.php
+++ routes/api.php
@@ -18,29 +18,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('articles', function() {
-    // If the Content-Type and Accept headers are set to 'application/json',
-    // this will return a JSON structure.
-    return Article::all();
-});
-
-Route::get('articles/{id}', function($id) {
-    return Article::find($id);
-});
-
-Route::post('articles', function(Request $request) {
-    return Article::create($request->all);
-});
-
-Route::put('articles/{id}', function(Request $request, $id) {
-    $article = Article::findOrFail($id);
-    $article->update($request->all());
-
-    return $article;
-});
-
-Route::delete('articles/{id}', function($id) {
-    Article::find($id)->delete();
-
-    return 204;
-});
+Route::get('articles', 'ArticleController@index');
+Route::get('articles/{id}', 'ArticleController@show');
+Route::post('articles', 'ArticleController@store');
+Route::put('articles/{id}', 'ArticleController@update');
+Route::delete('articles/{id}', 'ArticleController@delete');
